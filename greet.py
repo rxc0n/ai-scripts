abc76cd8b4a0c36f423aae1e4b2a6cc79e2fa993
x1 <108907332+rxc0n@users.noreply.github.com>
2023-11-18 10:25:40 -0500

    Create greet.py

---
+++ greet.py
@@ -0,0 +1,22 @@
+# Welcome message
+print("Hello everyone! Welcome to this AI script repository for fun.")
+
+# Disclaimer about inaccuracies
+print("Disclaimer: If anything is incorrect, blame it on GPT. I'm just a scripter!")
+
+# Fun function: Greet user
+def greet_user(name):
+    return f"Welcome, {name}! It's great to have you here."
+
+# Fun function: Generate a random number
+import random
+def generate_random_number():
+    return f"Here's a random number for you: {random.randint(1, 100)}"
+
+# Script events
+user_name = input("What's your name? ")
+print(greet_user(user_name))
+
+print(generate_random_number())
+
+# Feel free to add more fun functions and script events as you like!
